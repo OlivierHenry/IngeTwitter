21d81b3dfb5fef622d4288d28bf5ee41e5d67c3f
Olivier HENRY <olivier.henry@ingesup.com>
2012-02-14 17:18:38 +0100

    Suppression de la page Rails par defaut

--- Twitter/config/routes.rb
+++ Twitter/config/routes.rb
@@ -1,4 +1,9 @@
 Twitter::Application.routes.draw do
+
+  match '/contact', :to => 'pages#contact'
+  match '/about', 	:to => 'pages#about'
+  match '/help',	:to => 'pages#help'
+  match '/',		:to => 'pages#home'
   get "pages/home"
 
   get "pages/contact"
@@ -56,7 +61,7 @@ Twitter::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
