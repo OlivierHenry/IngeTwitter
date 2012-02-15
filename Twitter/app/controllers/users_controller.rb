d2c5eff8adc6d9d169557d2ff60f78868261ef9b
Olivier HENRY <olivier.henry@ingesup.com>
2012-02-15 11:54:49 +0100

    Inscription utilisateur finie

--- Twitter/app/controllers/users_controller.rb
+++ Twitter/app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def new
+  @user = User.new
   @title = "Inscription"
   end
   def show
@@ -7,4 +8,15 @@ class UsersController < ApplicationController
 	@title = @user.nom
   end
   
+  def create
+	@user = User.new(params[:user])
+	if @user.save
+		#succes d'enregistrement
+		flash[:success] = "Bienvenue dans Twitter Like ;)"
+		redirect_to @user
+	else
+		@titre = "Inscription"
+		render 'new'
+	end
+  end
 end
--- Twitter/config/application.rb
+++ Twitter/config/application.rb
@@ -44,5 +44,9 @@ module Twitter
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+	
+	# Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password]
+
   end
 end
